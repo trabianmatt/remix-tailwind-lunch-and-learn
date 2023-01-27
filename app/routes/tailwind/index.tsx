61478ec33281029ed6e461a07cb1c723b21acae5
Matt Dean <matt@trabian.com>
2023-01-27 12:00:14 -0500

    Initial commit

---
+++ app/routes/tailwind/index.tsx
@@ -0,0 +1,7 @@
+export default function TailwindPage() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="text-2xl">Welcome to the Tailwind Lunch and Learn!</div>
+    </div>
+  );
+}
